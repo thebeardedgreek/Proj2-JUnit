f33625005e8d16d5bd75feed2c05baaf51784acc
Anastasios Stamboulis <astamb1@students.towson.edu>
2021-02-19 22:29:53 -0500

    Fixed small error in code

--- src/edu/towson/cis/cosc442/project2/fibonacci/FibonacciTest.java
+++ src/edu/towson/cis/cosc442/project2/fibonacci/FibonacciTest.java
@@ -31,13 +31,13 @@ public class FibonacciTest {
 	 * Runs various equality tests against the Fibonacci class
 	 */
 	public void testFibonacci() {	
-		assertEquals(0, 0, fibonacci.fibonacci(0));
-		assertEquals(1, 1, fibonacci.fibonacci(1));
-		assertEquals(2, 1, fibonacci.fibonacci(2));
-		assertEquals(3, 2, fibonacci.fibonacci(3));
-		assertEquals(4, 3, fibonacci.fibonacci(4));
-		assertEquals(5, 5, fibonacci.fibonacci(5));
-		assertEquals(6, 8, fibonacci.fibonacci(6));
-		assertEquals(7, 13, fibonacci.fibonacci(7));
+		assertEquals(0, fibonacci.fibonacci(0), fibonacci.fibonacci(0));
+		assertEquals(1, fibonacci.fibonacci(1), fibonacci.fibonacci(1));
+		assertEquals(1, fibonacci.fibonacci(1), fibonacci.fibonacci(1));
+		assertEquals(2, fibonacci.fibonacci(2), fibonacci.fibonacci(2));
+		assertEquals(3, fibonacci.fibonacci(3), fibonacci.fibonacci(3));
+		assertEquals(5, fibonacci.fibonacci(5), fibonacci.fibonacci(5));
+		assertEquals(8, fibonacci.fibonacci(8), fibonacci.fibonacci(8));
+		assertEquals(13, fibonacci.fibonacci(13), fibonacci.fibonacci(13));
 	}
 }
